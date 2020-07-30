5a8ccc149cdef9a60b431640da22e916e9485985
siva6030 <68826545+siva6030@users.noreply.github.com>
2020-07-30 12:39:19 -0400

    Update 180886030_a04.c

--- 180886030_a04.c
+++ 180886030_a04.c
@@ -19,9 +19,18 @@ typedef struct thread //represents a single thread, you can add more members if 
 
 //you can add more functions here if required
 
-void* Run(void* t);//the thread function, the code executed by each thread
+void Run();//the thread function, the code executed by each thread
 int readFile(char* fileName);//function to read the file content and build array of threads
-char rff[5][8];
+void rq(char* c);
+void rl(char* c);
+void Ast();
+int safety();
+
+char rff[5][8];//resources from file
+int r[5][4];//max resources
+int ar[5][4];// allocated
+int nr[5][4];//needed
+int numOfR[4];
 
 int main(int argc, char *argv[])
 {
@@ -34,7 +43,8 @@ int main(int argc, char *argv[])
     //you can add some suitable code anywhere in main() if required
 
         Thread* threads = NULL;
-        int threadCount = readFile(argv[1]);
+        //argv[1]
+        int threadCount = readFile("sample4_in.txt");
         int numCus;
         printf("Number of Customes: ");
         scanf(" %d", &numCus);
@@ -76,7 +86,7 @@ int readFile(char* fileName)//do not modify this method
 
         char* command = NULL;
         int threadCount = 0;
-        char* fileCopy = (char*)malloc((strlen(fileContent)+1)*sizeof(char));
+                char* fileCopy = (char*)malloc((strlen(fileContent)+1)*sizeof(char));
         strcpy(fileCopy,fileContent);
         command = strtok(fileCopy,"\r\n");
         while(command!=NULL)
@@ -85,7 +95,8 @@ int readFile(char* fileName)//do not modify this method
                 command = strtok(NULL,"\r\n");
         }
 //      *threads = (Thread*) malloc(sizeof(Thread)*threadCount);
-char* lines[threadCount];
+
+        char* lines[threadCount];
         command = NULL;
         int i=0;
         command = strtok(fileContent,"\r\n");
@@ -105,18 +116,15 @@ char* lines[threadCount];
                 while(token!=NULL)
                 {
 //if you have extended the Thread struct then here
-//you can do initialization of those additional members
+      //you can do initialization of those additional members
 //or any other action on the Thread members
 //                      printf("%s\n",token);
+
                         strcpy(rff[k], token);
-/*
-                        (*threads)[k].state=0;
-                        if(j==0)
-                                strcpy((*threads)[k].tid,token);
-                        if(j==1)
-                                (*threads)[k].startTime=atoi(token);
+                        r[k][j] = atoi(token);
+                        nr[k][j] = atoi(token);
+                        ar[k][j] =  0;
                         j++;
-*/
                         token = strtok(NULL," ");
 
                 }
@@ -127,8 +135,24 @@ char* lines[threadCount];
 }
 
 
-void* Run(void* t)//implement this function in a suitable way
+void run()//implement this function in a suitable way
 {
+
+}
+
+void rq(char* c){
+
+}
+
+void rl(char* c){
+
+}
+
+void Ast(){
+
+}
+
+int safety(){
 
 }
 
